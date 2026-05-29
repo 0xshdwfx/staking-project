40bcf10df9311caafc025f545ff530a82a8a3656
0xshdwfx <0x@0xs.to>
2026-05-29 19:34:08 +0100

    RewardToken contract created with constructor and mint() function

---
+++ src/RewardToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract RewardToken is ERC20, Ownable {
+    uint256 private constant INITIAL_MINT_VALUE = 1e6 * 1e18; // 1 million tokens with 18 decimals
+
+    constructor() ERC20("Reward Token", "RWT") Ownable(msg.sender) {
+        _mint(msg.sender, INITIAL_MINT_VALUE);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}
