cbbf34e13681505cf8967e0b89376099f81a6c63
0xshdwfx <0x@0xs.to>
2026-06-01 12:54:24 +0100

    created Staking.sol

---
+++ src/Staking.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+
+contract Staking is Ownable, ReentrancyGuard, Pausable {
+    constructor() Ownable(msg.sender) {}
+}
