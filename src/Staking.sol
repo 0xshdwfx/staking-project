8225b5c7d391ecd57b463a55b5786499f0b60d39
0xshdwfx <0x@0xs.to>
2026-06-01 17:08:11 +0100

    feat(Staking): add constructor with immutable token reference
    - Add input validation with custom InvalidTokenAddress error
    
    - Initialize Ownable with msg.sender
    
    - Store RewardToken as immutable state variable

--- src/Staking.sol
+++ src/Staking.sol
@@ -5,7 +5,26 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Pausable.sol";
+import {RewardToken} from "src/RewardToken.sol";
 
 contract Staking is Ownable, ReentrancyGuard, Pausable {
-    constructor() Ownable(msg.sender) {}
+    /////////////////
+    /// Errors //////
+    /////////////////
+
+    error InvalidTokenAddress();
+
+    ////////////////////////
+    /// State Variables ///
+    //////////////////////
+
+    RewardToken public immutable REWARD_TOKEN;
+
+    ///////////////////
+    /// Constructor ///
+    ///////////////////
+    constructor(address tokenAddress) Ownable(msg.sender) {
+        if (tokenAddress == address(0)) revert InvalidTokenAddress();
+        REWARD_TOKEN = RewardToken(tokenAddress);
+    }
 }
