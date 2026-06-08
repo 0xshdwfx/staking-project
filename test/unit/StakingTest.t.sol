bc0713db79441404c77ee160ebed5dc8ff471edb
0xshdwfx <0x@0xs.to>
2026-06-08 14:59:59 +0100

    feat(StakingTest): add StakingTest.t.sol contract

---
+++ test/unit/StakingTest.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {Test} from "forge-std/Test.sol";
+import {Staking} from "src/Staking.sol";
+
+contract StakingTest is Test {
+    Staking public staking;
+}
