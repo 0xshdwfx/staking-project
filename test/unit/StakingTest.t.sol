54cf73ea68695a5dd716ee42e19cfd037b730450
0xshdwfx <0x@0xs.to>
2026-06-09 18:15:46 +0100

    test(Staking): initialise test setup with contracts and users
    - deploy ERC20Mock staking token, RewardToken, and Staking contract
    
    - create test user with initial token balance and approval
    
    - prepare test environment for stake, unstake, and claimReward tests

--- test/unit/StakingTest.t.sol
+++ test/unit/StakingTest.t.sol
@@ -3,7 +3,26 @@ pragma solidity ^0.8.26;
 
 import {Test} from "forge-std/Test.sol";
 import {Staking} from "src/Staking.sol";
+import {RewardToken} from "src/RewardToken.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract StakingTest is Test {
+    ERC20Mock stakingToken;
+    RewardToken public rewardToken;
     Staking public staking;
+
+    // test user
+    address public user = makeAddr("user");
+    uint256 public constant STARTING_USER_BALANCE = 10e18;
+
+    function setUp() public {
+        stakingToken = new ERC20Mock();
+        rewardToken = new RewardToken();
+        staking = new Staking(address(stakingToken), address(rewardToken));
+
+        stakingToken.mint(user, STARTING_USER_BALANCE);
+
+        vm.prank(user);
+        stakingToken.approve(address(staking), type(uint256).max); // type(uint256).max - testing purposes only
+    }
 }
